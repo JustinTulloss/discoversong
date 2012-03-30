924d3ced27c0133c85dc96054684017abbb29410
Eugene Efremov <eugene@rd.io>
2012-03-30 14:51:08 -0700

    try setting host

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ class root:
       dburl = os.environ['HEROKU_SHARED_POSTGRESQL_JADE_URL']
       print dburl
       db = web.database(dburl=dburl,
-          dbn='postgres', user='tguaspklkhnrpn', pw='4KBnjLB1n5wbuvzNB4p7DyQEpF', db='vivid_winter_30977')
+          dbn='postgres', host='pg60.sharedpg.heroku.com', user='tguaspklkhnrpn', pw='4KBnjLB1n5wbuvzNB4p7DyQEpF', db='vivid_winter_30977')
       
       result = db.select(['email_from_address', 'email_to_address', 'rdio_playlist_id'], where="rdio_user_id=%i" % user_id)
       print result
